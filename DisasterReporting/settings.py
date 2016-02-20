2427f05f2d16c7eff096a353af9c45e37263ab3c
Lucy Tan <lucyktan@gmail.com>
2016-02-19 23:30:15 -0500

    Connect to MySQL database

--- DisasterReporting/settings.py
+++ DisasterReporting/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DisasterReporting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'disasterreporting',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
