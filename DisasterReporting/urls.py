1f92b70969a255586f6efafddd88ed7679d6ff63
Lucy Tan <lucyktan@gmail.com>
2016-02-21 20:38:56 -0500

    Add User and Reports models and setup view

--- DisasterReporting/urls.py
+++ DisasterReporting/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from DisasterReporting.views import hello
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
 ]
+
+"""
+browser request -> setting ROOT_URLCONF -> urls.py -> views.py --> template
+"""
