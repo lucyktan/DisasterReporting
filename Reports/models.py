424fab9cba768999ed05010b9841cddc3caa2ace
kmp74 <kmp74@case.edu>
2016-04-03 21:28:11 -0400

    Merge branch 'UpdatedForm'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- Reports/models.py
+++ Reports/models.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import datetime
 
-#Create your models here.
+"""Historical data from FEMA's PDA documents"""
 class disaster_history(models.Model):
     dishist_id = models.IntegerField()
     disasterNumber = models.IntegerField()
@@ -15,6 +15,7 @@ class disaster_history(models.Model):
     incidentType = models.CharField(max_length=16)
     year = models.IntegerField()
 
+"""Property values data from Zillow"""
 class homevalue(models.Model):
     class Meta:
         app_label = 'Reports'
@@ -24,6 +25,8 @@ class homevalue(models.Model):
     rent = models.IntegerField()
     def __str__(self):
         return homevalue.content
+
+"""Historical disaster data from property owners provided by FEMA"""       
 class owners(models.Model):
     owners_id = models.IntegerField()
     disasterNumber = models.IntegerField()
@@ -46,6 +49,7 @@ class owners(models.Model):
     ApprovedBetween25001AndMax = models.IntegerField()
     TotalMaxGrants = models.IntegerField()
 
+"""Historical disaster data from property renters provided by FEMA"""
 class renters(models.Model):
     renters_id = models.IntegerField()
     disasterNumber = models.IntegerField()
@@ -66,51 +70,111 @@ class renters(models.Model):
     ApprovedBetween25001AndMax = models.IntegerField()
     TotalMaxGrants = models.IntegerField()
 
-# Create your models here.
+"""Submitted information from the Report Damage form"""
 class Report(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    street_address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=50)
-    zipcode = models.IntegerField()
-    type_of_residence = models.CharField(max_length=50)
-    type_of_occupancy = models.CharField(max_length=50)
-    type_of_disaster = models.CharField(max_length=50)
-    date_of_disaster = models.DateField(auto_now=False, auto_now_add=False)
-    insured = models.IntegerField()
-    mortgage = models.IntegerField()
-    owned_less_than_30_years=models.IntegerField()
-    predisaster_value = models.DecimalField(max_digits=50, decimal_places=2)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
+    street_address = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    state = models.CharField(max_length=50, default='')
+    zipcode = models.IntegerField(default=0)
+    type_of_residence = models.CharField(max_length=50, default='')
+    type_of_occupancy = models.CharField(max_length=50, default='')
+    type_of_disaster = models.CharField(max_length=50, default='')
+    date_of_disaster = models.DateField(auto_now=False, auto_now_add=False, default=datetime.date.today())
+    insured = models.IntegerField(default=2)
+    mortgage = models.IntegerField(default=2)
+    owned_less_than_30_years=models.IntegerField(default=2)
+    predisaster_value = models.DecimalField(max_digits=50, decimal_places=2, default=0)
 
-    normal_water = models.CharField(max_length=50)
+    water_damage = models.IntegerField(default=2)
 
-    normal_destroyed_0 = models.IntegerField()
-    normal_destroyed_1 = models.IntegerField()
-    normal_destroyed_2 = models.IntegerField()
-    normal_destroyed_3 = models.IntegerField()
+    water_mobilehome = models.IntegerField(default=2)
 
-    normal_major_0 = models.IntegerField()
-    normal_major_1 = models.IntegerField()
-    normal_major_2 = models.IntegerField()
-    normal_major_3 = models.IntegerField()
+    water_mobilehome_minor = models.IntegerField(default=2)
 
-    normal_minor_0 = models.IntegerField()
-    normal_minor_1 = models.IntegerField()
-    normal_minor_2 = models.IntegerField()
-    normal_minor_3 = models.IntegerField()
+    water_mobilehome_major_plywood = models.IntegerField(default=2)
+    water_mobilehome_major_plywood_yes = models.IntegerField(default=2)
+    water_mobilehome_major_nonplywood = models.IntegerField(default=2)
+    water_mobilehome_destroyed = models.IntegerField(default=2)
 
-    manufactured_destroyed_0 = models.IntegerField()
-    manufactured_destroyed_1 = models.IntegerField()
-    manufactured_destroyed_2 = models.IntegerField()
-    manufactured_destroyed_3 = models.IntegerField()
+    water_conventionalhome_minor = models.IntegerField(default=2)
+    water_conventionalhome_major = models.IntegerField(default=2)
+    water_conventionalhome_destroyed = models.IntegerField(default=2)
 
-    manufactured_major_0 = models.IntegerField()
-    manufactured_major_1 = models.IntegerField()
-    manufactured_major_2 = models.IntegerField()
-    manufactured_major_3 = models.IntegerField()
+    sewage = models.IntegerField(default=2)
 
-    manufactured_minor_0 = models.IntegerField()
-    manufactured_minor_1 = models.IntegerField()
-    manufactured_minor_2 = models.IntegerField()
-    manufactured_minor_3 = models.IntegerField()
+    minor10_0 = models.IntegerField(default=2)
+    minor10_1 = models.IntegerField(default=2)
+    minor10_2 = models.IntegerField(default=2)
+
+    major20_0 = models.IntegerField(default=2)
+    major20_1 = models.IntegerField(default=2)
+    major20_2 = models.IntegerField(default=2)
+
+    major30_0 = models.IntegerField(default=2)
+    major30_1 = models.IntegerField(default=2)
+    major30_2 = models.IntegerField(default=2)
+    major30_3 = models.IntegerField(default=2)
+
+    major40_0 = models.IntegerField(default=2)
+    major40_1 = models.IntegerField(default=2)
+    major40_2 = models.IntegerField(default=2)
+
+    major50_0 = models.IntegerField(default=2)
+    major50_1 = models.IntegerField(default=2)
+    major50_2 = models.IntegerField(default=2)
+
+    major60_0 = models.IntegerField(default=2)
+    major60_1 = models.IntegerField(default=2)
+
+    major74_0 = models.IntegerField(default=2)
+    major74_1 = models.IntegerField(default=2)
+    major74_2 = models.IntegerField(default=2)
+
+    destroyed80_0 = models.IntegerField(default=2)
+    destroyed80_1 = models.IntegerField(default=2)
+    destroyed80_2 = models.IntegerField(default=2)
+    destroyed80_3 = models.IntegerField(default=2)
+    destroyed80_4 = models.IntegerField(default=2)
+
+    destroyed90_0 = models.IntegerField(default=2)
+    destroyed90_1 = models.IntegerField(default=2)
+
+    destroyed100_0 = models.IntegerField(default=2)
+    destroyed100_1 = models.IntegerField(default=2)
+
+    perDam= models.DecimalField(max_digits=20,decimal_places=4, default=-1.0000)
+    estimated_damage= models.DecimalField(max_digits=20,decimal_places=2, default=-1.00)
+    fema_disaster_number = models.IntegerField(default=-1)
+    # normal_water = models.CharField(max_length=50)
+
+    # normal_destroyed_0 = models.IntegerField()
+    # normal_destroyed_1 = models.IntegerField()
+    # normal_destroyed_2 = models.IntegerField()
+    # normal_destroyed_3 = models.IntegerField()
+
+    # normal_major_0 = models.IntegerField()
+    # normal_major_1 = models.IntegerField()
+    # normal_major_2 = models.IntegerField()
+    # normal_major_3 = models.IntegerField()
+
+    # normal_minor_0 = models.IntegerField()
+    # normal_minor_1 = models.IntegerField()
+    # normal_minor_2 = models.IntegerField()
+    # normal_minor_3 = models.IntegerField()
+
+    # manufactured_destroyed_0 = models.IntegerField()
+    # manufactured_destroyed_1 = models.IntegerField()
+    # manufactured_destroyed_2 = models.IntegerField()
+    # manufactured_destroyed_3 = models.IntegerField()
+
+    # manufactured_major_0 = models.IntegerField()
+    # manufactured_major_1 = models.IntegerField()
+    # manufactured_major_2 = models.IntegerField()
+    # manufactured_major_3 = models.IntegerField()
+
+    # manufactured_minor_0 = models.IntegerField()
+    # manufactured_minor_1 = models.IntegerField()
+    # manufactured_minor_2 = models.IntegerField()
+    # manufactured_minor_3 = models.IntegerField()
