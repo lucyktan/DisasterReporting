2603b9ca8ffeb506a2d4fd0dd6e092e3669220c7
kmp74 <kmp74@case.edu>
2016-04-10 23:03:38 -0400

    Gives fema disaster number for new form entries

--- Reports/views.py
+++ Reports/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from Reports.models import Report
 from forms import DisasterForm
+import datetime
 
 """Renders the Report Damage form depending on whether the user is logged in"""
 
@@ -21,7 +22,18 @@ def get_form(request):
     return render(request, 'form.html', {'form': form})
 
 def make_report(form):
-    return Report(first_name=form.cleaned_data['first_name'],
+   inpState = form.cleaned_data['state']
+   inpDisType  = form.cleaned_data['type_of_disaster']
+   inpDate = form.cleaned_data['date_of_disaster']
+   start = inpDate + datetime.timedelta(days=-4)
+   end = inpDate + datetime.timedelta(days= 4)
+   num = Report.objects.filter(date_of_disaster__range = (start,end), state=inpState, type_of_disaster=inpDisType)
+   if not num.exists():
+        number = Report.objects.raw('SELECT id, max(fema_disaster_number) FROM disaster.reports_report')[0].fema_disaster_number + 1
+   else:
+        number = num.raw('SELECT fema_disaster_number, re FROM disaster.reports_report')[0].fema_disaster_number
+
+   return Report(first_name=form.cleaned_data['first_name'],
 last_name=form.cleaned_data['last_name'],
 street_address=form.cleaned_data['street_address'],
 city=form.cleaned_data['city'],
@@ -91,33 +103,9 @@ destroyed90_0 = form.cleaned_data['destroyed90_0'],
 destroyed90_1 = form.cleaned_data['destroyed90_1'],
 
 destroyed100_0 = form.cleaned_data['destroyed100_0'],
-destroyed100_1 = form.cleaned_data['destroyed100_1'])
+destroyed100_1 = form.cleaned_data['destroyed100_1'],
+fema_disaster_number=number)
 
-# normal_water=form.cleaned_data['normal_water'],
-# normal_destroyed_0=form.cleaned_data['normal_destroyed_0'],
-# normal_destroyed_1=form.cleaned_data['normal_destroyed_1'],
-# normal_destroyed_2=form.cleaned_data['normal_destroyed_2'],
-# normal_destroyed_3=form.cleaned_data['normal_destroyed_3'],
-# normal_major_0=form.cleaned_data['normal_major_0'],
-# normal_major_1=form.cleaned_data['normal_major_1'],
-# normal_major_2=form.cleaned_data['normal_major_2'],
-# normal_major_3=form.cleaned_data['normal_major_3'],
-# normal_minor_0=form.cleaned_data['normal_minor_0'],
-# normal_minor_1=form.cleaned_data['normal_minor_1'],
-# normal_minor_2=form.cleaned_data['normal_minor_2'],
-# normal_minor_3=form.cleaned_data['normal_minor_3'],
-# manufactured_destroyed_0=form.cleaned_data['manufactured_destroyed_0'],
-# manufactured_destroyed_1=form.cleaned_data['manufactured_destroyed_1'],
-# manufactured_destroyed_2=form.cleaned_data['manufactured_destroyed_2'],
-# manufactured_destroyed_3=form.cleaned_data['manufactured_destroyed_3'],
-# manufactured_major_0=form.cleaned_data['manufactured_major_0'],
-# manufactured_major_1=form.cleaned_data['manufactured_major_1'],
-# manufactured_major_2=form.cleaned_data['manufactured_major_2'],
-# manufactured_major_3=form.cleaned_data['manufactured_major_3'],
-# manufactured_minor_0=form.cleaned_data['manufactured_minor_0'],
-# manufactured_minor_1=form.cleaned_data['manufactured_minor_1'],
-# manufactured_minor_2=form.cleaned_data['manufactured_minor_2'],
-# manufactured_minor_3=form.cleaned_data['manufactured_minor_3'])
 
 def show_results(request):
     return render(request, 'results.html')
