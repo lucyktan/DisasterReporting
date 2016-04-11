1168d65e4f10c3df6b374d7536063565b9bbc4f4
Ben <benjamin.marks@case.edu>
2016-04-10 21:30:09 -0400

    Added default value for latitude and longitude in Report

--- Reports/models.py
+++ Reports/models.py
@@ -148,8 +148,8 @@ class Report(models.Model):
     perDam= models.DecimalField(max_digits=20,decimal_places=4, default=-1.0000)
     estimated_damage= models.DecimalField(max_digits=20,decimal_places=2, default=-1.00)
     fema_disaster_number = models.IntegerField(default=-1)
-    latitude=models.DecimalField(max_digits=15,decimal_places=10)
-    longitude=models.DecimalField(max_digits=15,decimal_places=10)
+    latitude=models.DecimalField(max_digits=15,decimal_places=10,default=-9999)
+    longitude=models.DecimalField(max_digits=15,decimal_places=10,default=-9999)
 
 class MapData():
     locations=[]
